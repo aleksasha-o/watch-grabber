96574c9fc78e99105544745b09cf9312cb57f05b
OOrlova <alexandra.volosko@gmail.com>
2022-04-06 12:46:18 +0200

    Test

--- app/services/browser.rb
+++ app/services/browser.rb
@@ -32,7 +32,6 @@ class Browser
     begin
       raise NotFoundError if browser.at_css(tag).nil?
     rescue NotFoundError, Ferrum::NodeNotFoundError
-      clear_cache
       sleep RETRY_INTERVAL
       clear_cache
       load_time += RETRY_INTERVAL
