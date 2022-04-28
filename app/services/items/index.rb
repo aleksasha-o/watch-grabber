91f36b656aea9fd1c4653eb8fab933ab7608299b
OOrlova <alexandra.volosko@gmail.com>
2022-04-28 13:19:06 +0200

    Add new scope Filters and change sort to use case

--- app/services/items/index.rb
+++ app/services/items/index.rb
@@ -9,12 +9,7 @@ module Items
     def items
       return @items if @items
 
-      @items = Item.all
-
-      search_items
-      sort_items
-
-      @items
+      @items = Filters::Items.filter(params: @params)
     end
 
     def brands_for_select
@@ -27,26 +22,6 @@ module Items
 
     def default_max_price
       Item.pluck(:price)&.max&.round
-    end
-
-    private
-
-    def search_items
-      @items = @items.by_brands(@params[:brands])
-      @items = @items.by_model(@params[:model])
-      @items = @items.min_max_price(min_price: @params[:price_gteq], max_price: @params[:price_lteq])
-      @items = @items.by_dial_color(@params[:dial_color])
-      @items = @items.by_case_material(@params[:case_material])
-      @items = @items.by_case_dimensions(@params[:case_dimensions])
-      @items = @items.by_bracelet_material(@params[:bracelet_material])
-      @items = @items.by_movement_type(@params[:movement_type])
-    end
-
-    def sort_items
-      @items = @items.ordered_by_brand_asc if @params[:sort] == 'brandASC'
-      @items = @items.ordered_by_brand_desc if @params[:sort] == 'brandDESC'
-      @items = @items.ordered_by_price_asc if @params[:sort] == 'priceASC'
-      @items = @items.ordered_by_price_desc if @params[:sort] == 'priceDESC'
     end
   end
 end
