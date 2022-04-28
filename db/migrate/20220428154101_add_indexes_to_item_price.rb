ef697ffe387eaefa07a5c9ad0c20232eb4c6f0fa
OOrlova <alexandra.volosko@gmail.com>
2022-04-28 19:18:57 +0200

    Add indexes for all fields that are filtered

--- db/migrate/20220428154101_add_indexes_to_item_price.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexesToItemPrice < ActiveRecord::Migration[6.0]
-  def change
-    add_index :items, :price
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_28_154101) do
+ActiveRecord::Schema.define(version: 2022_04_28_170129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -38,7 +38,14 @@ ActiveRecord::Schema.define(version: 2022_04_28_154101) do
     t.enum "currency", null: false, enum_type: "item_currency"
     t.string "external_id"
     t.string "image_uri"
+    t.index ["bracelet_material"], name: "index_items_on_bracelet_material"
+    t.index ["brand"], name: "index_items_on_brand"
+    t.index ["case_dimensions"], name: "index_items_on_case_dimensions"
+    t.index ["case_material"], name: "index_items_on_case_material"
+    t.index ["dial_color"], name: "index_items_on_dial_color"
     t.index ["external_id"], name: "index_items_on_external_id", unique: true
+    t.index ["model"], name: "index_items_on_model"
+    t.index ["movement_type"], name: "index_items_on_movement_type"
     t.index ["price"], name: "index_items_on_price"
   end
 
