fbd78a5ae2f83c08c1ff1cd30ed6fb6961661ea0
OOrlova <alexandra.volosko@gmail.com>
2022-05-03 17:26:41 +0200

    Fix Factory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,18 +3,18 @@
 FactoryBot.define do
   factory :item do
     type              { %w[HodinkeeItem CrownandcaliberItem BobswatchesItem].sample }
-    model             { Faker::Commerce.product_name }
     price             { Faker::Commerce.price(range: 0..100_000.0, as_string: true) }
     currency          { %w[usd eur uah].sample }
-    dial_color        { Faker::Commerce.color }
-    case_material     { Faker::Commerce.material }
     case_dimensions   { Faker::Number.between(from: 26.0, to: 60.0).round(1).to_s }
-    bracelet_material { Faker::Commerce.material }
-    movement_type     { Faker::Lorem.word }
     external_id       { Faker::Internet.uuid }
     image_uri         { Faker::Internet.url  }
 
     sequence(:brand) { |n| "#{Faker::Commerce.brand}#{n}" }
+    sequence(:model) { |n| "#{Faker::Commerce.product_name}#{n}" }
+    sequence(:dial_color) { |n| "#{Faker::Commerce.color}#{n}" }
+    sequence(:case_material) { |n| "#{Faker::Commerce.material}#{n}" }
+    sequence(:bracelet_material) { |n| "#{Faker::Commerce.material}#{n}" }
+    sequence(:movement_type) { |n| "#{Faker::Lorem.word}#{n}" }
     initialize_with { (type.safe_constantize || Item).new }
   end
 end
