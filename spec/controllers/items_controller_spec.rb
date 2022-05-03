1b1473ef0ff37815125a9e967ed4708417459a6a
OOrlova <alexandra.volosko@gmail.com>
2022-05-03 13:20:00 +0200

    Fix Items controller Rspec

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -4,11 +4,11 @@ describe ItemsController, type: :controller do
   describe '#index' do
     subject { get :index }
 
-    let!(:items)  { create_list(:item, rand(4..7)) }
-    let(:expected_list) { items.sort_by(&:brand) }
+    before do
+      allow(Items::Index).to receive(:new).and_return({ controller: 'items', action: 'index' })
+      subject
+    end
 
-    before { subject }
-
-    it { expect(assigns(:items)).to eq(expected_list) }
+    it { expect(assigns(:data)).to eq({ controller: 'items', action: 'index' }) }
   end
 end
